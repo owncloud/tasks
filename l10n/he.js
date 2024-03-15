44a570ede8bf8026e258e904cbe9adfbc4f18098
ownClouders <devops@owncloud.com>
2024-03-15 00:07:50 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -111,4 +111,4 @@ OC.L10N.register(
     "Task not found!" : "פעילות לא אותרה!",
     "add a subtask to" : "הוספת תת-פעילות ל-"
 },
-"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
